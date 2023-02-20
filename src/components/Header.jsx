81880a1b8eec8cbb61b44cb5f4193b1d3b8742de
mindyaaa <107449479+mindyaaa@users.noreply.github.com>
2023-02-20 15:48:21 +0900

    헤더 설정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import {RiShoppingBag3Line} from 'react-icons/ri';
+import {BsFillPencilFill} from 'react-icons/bs';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
     return (
-        <hearder>
-            나?헤더!
-        </hearder>
+        <header className='p-2 flex justify-between border-b border-gray-300'>
+            <Link to='/'
+            className='flex items-center text-4xl text-brand'
+            >
+                <RiShoppingBag3Line />
+                <h1>Shoppy</h1>
+            </Link>
+            <nav
+            className='flex items-center gap-4 font-semibold'>
+                <Link to='/products'>products</Link>
+                <Link to='/carts'>carts</Link>
+                <Link to='/products/new'
+                className='tex-2xl'
+                >
+                    <BsFillPencilFill />
+                </Link>
+            </nav>
+        </header>
     );
 }
 
